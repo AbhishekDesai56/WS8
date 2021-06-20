494de3a0489c44b0fa8e571213092f87d317d3ab
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-06-21 00:55:00 +0530

    Display Employee Details from JSON Object including the Department

--- js/test_home.js
+++ js/test_home.js
@@ -5,20 +5,62 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {
   const headerHtml = "<th></th><th>Name</th> <th>Gender</th><th>Department</th>" +
                      "<th>Salary</th> <th>Start Date</th> <th>Actions</th>";
-  const innerHtml = `${headerHtml}        
+  let innerHtml =`${headerHtml}`;
+  let empPayrollList = createEmployeePayrollJSON();
+  for(const empPayrollData of empPayrollList) {
+   innerHtml = `${innerHtml}        
   <tr>
-    <td><img class="profile" src="../assets/profile-pictures/profile2.jpg" alt=""></td>
-    <td>Abhishek</td>
-    <td>Male</td>
-    <td><div class="dept-label">HR</div>
-      <div class="dept-label">Finance</div></td>
-      <td>300000</td>
-      <td>1 June 2021</td>
-      <td>
-        <img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.PNG" alt="delete">
-        <img id="1" onclick="remove(this)" src="../assets/icons/create-black-18dp.PNG" alt="edit">
-      </td>
+    <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+    <td>${empPayrollData._name}</td>
+    <td>${empPayrollData._salary}</td>
+    <td>${getDeptHtml(empPayrollData._department)}</td>
+    <td>${empPayrollData._salary}</td>
+    <td>${empPayrollData._startDate}</td>
+    <td>
+      <img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.PNG" alt="delete">
+      <img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/create-black-18dp.PNG" alt="edit">
+    </td>
   </tr>
   `;
+  }
   document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+    {
+      _name: 'Abhishek',
+      _gender: 'male',
+      _department: [
+        'Engineer',
+        'Finance',
+      ],
+      _salary: '500000',
+      _startDate: '21 June 2021',
+      _note: '',
+      _id: new Date().getTime(),
+      _profilePic: '../assets/profile-pictures/profile2.jpg' 
+    },
+    {
+      _name: 'Mary John Jain',
+      _gender: 'female',
+      _department: [
+        'Sales',
+      ],
+      _salary: '400000',
+      _startDate: '21 June 2021',
+      _note: '',
+      _id: new Date().getTime() + 1,
+      _profilePic: '../assets/profile-pictures/profile1.jpg' 
+    },
+  ];
+  return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+  let deptHtml = '';
+  for(const dept of deptList) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
 }
